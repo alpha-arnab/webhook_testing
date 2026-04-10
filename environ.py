9ee5c88ea5a72bac33c92bf3465bb08c2bb4f14b
alpha-arnab <arcadearnab@gmail.com>
2026-04-10 12:18:56 +0530

    Update environ.py

--- environ.py
+++ environ.py
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDsmeApGUc_Zr-qoI-74Ow6xT-XzBtqjoY",
-  authDomain: "posti4-3554b.firebaseag.com",
-  projectId: "posti4-3554b",
-  storageBucket: "posti4-3554b.firasestorage.app",
-  messagingSenderId: "90148381",
-  appId: "1:901483879161:rte:a08dcece12b3fd2be16028"
-};
+
