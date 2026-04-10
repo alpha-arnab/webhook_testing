ee7870725f92e70fea8a7720fa96ca7bf9eb1c9d
alpha-arnab <arcadearnab@gmail.com>
2026-04-10 11:53:12 +0530

    fix: replace hardcoded secret with env variable

--- environ.py
+++ environ.py
@@ -1,8 +1,10 @@
+```javascript
 const firebaseConfig = {
-  apiKey: "AIzaSyDsmeApGUc_Zr-qoI-74Ow6xT-XzBtqjoY",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "posti4-3554b.firebaseag.com",
   projectId: "posti4-3554b",
   storageBucket: "posti4-3554b.firasestorage.app",
   messagingSenderId: "90148381",
   appId: "1:901483879161:rte:a08dcece12b3fd2be16028"
 };
+```
